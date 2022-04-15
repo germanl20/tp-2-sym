96a011924ef06dbafd36ef34649a644983ac173a
german Lopez <germanlopezz16@hotmail.com>
2022-04-15 20:12:44 -0300

    Modificacion ensamblador para aceptar las nuevas instrucciones

--- ensamblador.py
+++ ensamblador.py
@@ -2,6 +2,7 @@ import re
 
 from ejecutable import Ejecutable
 from instrucciones.add import Add
+from instrucciones.call import Call
 from instrucciones.cmp import Cmp
 from instrucciones.dec import Dec
 from instrucciones.etiqueta import Etiqueta
@@ -9,6 +10,9 @@ from instrucciones.inc import Inc
 from instrucciones.jmp import Jmp
 from instrucciones.jnz import Jnz
 from instrucciones.mov import Mov
+from instrucciones.pop import Pop
+from instrucciones.push import Push
+from instrucciones.ret import Ret
 
 class Ensamblador:
 
@@ -76,68 +80,93 @@ class Ensamblador:
 
     
     def __procesarInstruccion(self, linea, numeroDeLinea):
-        match = re.search('^\s*(mov|add|cmp|inc|dec|jmp|jnz)\s+(.*)', linea)
+        if(re.search('^\s*(ret)\s*$', linea)):
+            instruccionNueva = Ret()
+            self.ejecutable.listaInstrucciones.append(instruccionNueva)
+            self.ejecutable.listaInstruccionesCodFuente.append(linea)
         
-        if (match):
-            instruccion = match.group(1)
-            parametros = match.group(2)
-
-            #Si es una instruccion que lleve dos parametros
-            if(instruccion == 'mov' or instruccion == 'add' or instruccion == 'cmp'):
-                #Obtengo los parametros
-                match = re.search('^(ax|bx|cx|dx)\s*,\s*(ax|bx|cx|dx|\d+)\s*$', parametros)
-                if (match):
-                    if(instruccion == 'mov'):
-                        instruccionNueva = Mov(match.group(1), match.group(2))
-                    
-                    elif(instruccion == 'add'):
-                        instruccionNueva = Add(match.group(1), match.group(2))
-
-                    elif(instruccion == 'cmp'):
-                        instruccionNueva = Cmp(match.group(1), match.group(2))
-                    
-                    self.ejecutable.listaInstrucciones.append(instruccionNueva)
-                    self.ejecutable.listaInstruccionesCodFuente.append(linea)
-                
-                #Si dio error por parametros invalidos
-                else:
-                    self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametros no validos"
-            
-            elif(instruccion == 'jmp' or instruccion == 'jnz'):
-                #Obtengo los parametros
-                match = re.search('^(\w+)\s*$', parametros)
-                if(match):
-                    if(instruccion == 'jmp'):
-                        instruccionNueva = Jmp(match.group(1))
-                    
-                    elif(instruccion == 'jnz'):
-                        instruccionNueva = Jnz(match.group(1))
-                    
-                    self.ejecutable.listaInstrucciones.append(instruccionNueva)
-                    self.ejecutable.listaInstruccionesCodFuente.append(linea)
-                
-                else:
-                    self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametro no valido - Debe ser una sola palabra que represente el nombre de una etiqueta"
-            
-            else:
-                #Obtengo los parametros
-                match = re.search('^(ax|bx|cx|dx)\s*$', parametros)
-                if(match):
-                    if(instruccion == 'inc'):
-                        instruccionNueva = Inc(match.group(1))
-                    
-                    elif(instruccion == 'dec'):
-                        instruccionNueva = Dec(match.group(1))
-                    
-                    self.ejecutable.listaInstrucciones.append(instruccionNueva)
-                    self.ejecutable.listaInstruccionesCodFuente.append(linea)
-                
-                else:
-                    self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametro no valido - Debe ser 'ax', 'bx', 'cx' o 'dx'"
-        
-        #Si dio error por instruccion mal escrita o no valida
         else:
-            self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": instruccion no valida"
+            match = re.search('^\s*(mov|add|cmp|inc|dec|jmp|jnz|call|push|pop)\s+(.*)', linea)
+            
+            if (match):
+                instruccion = match.group(1)
+                parametros = match.group(2)
+
+                #Si es una instruccion que lleve dos parametros
+                if(instruccion == 'mov' or instruccion == 'add' or instruccion == 'cmp'):
+                    #Obtengo los parametros
+                    match = re.search('^(ax|bx|cx|dx)\s*,\s*(ax|bx|cx|dx|\d+)\s*$', parametros)
+                    if (match):
+                        if(instruccion == 'mov'):
+                            instruccionNueva = Mov(match.group(1), match.group(2))
+                        
+                        elif(instruccion == 'add'):
+                            instruccionNueva = Add(match.group(1), match.group(2))
+
+                        elif(instruccion == 'cmp'):
+                            instruccionNueva = Cmp(match.group(1), match.group(2))
+                        
+                        self.ejecutable.listaInstrucciones.append(instruccionNueva)
+                        self.ejecutable.listaInstruccionesCodFuente.append(linea)
+                    
+                    #Si dio error por parametros invalidos
+                    else:
+                        self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametros no validos"
+                
+                elif(instruccion == 'jmp' or instruccion == 'jnz'  or instruccion == 'call'):
+                    #Obtengo los parametros
+                    match = re.search('^(\w+)\s*$', parametros)
+                    if(match):
+                        if(instruccion == 'jmp'):
+                            instruccionNueva = Jmp(match.group(1))
+                        
+                        elif(instruccion == 'jnz'):
+                            instruccionNueva = Jnz(match.group(1))
+                        
+                        elif(instruccion == 'call'):
+                            instruccionNueva = Call(match.group(1), int(numeroDeLinea) + 1)
+                        
+                        self.ejecutable.listaInstrucciones.append(instruccionNueva)
+                        self.ejecutable.listaInstruccionesCodFuente.append(linea)
+                    
+                    else:
+                        self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametro no valido - Debe ser una sola palabra que represente el nombre de una etiqueta"
+            
+                elif(instruccion == 'push'):
+                    #Obtengo los parametros
+                    match = re.search('^(ax|bx|cx|dx|\d+)\s*$', parametros)
+                    if(match):
+                        instruccionNueva = Push(match.group(1))
+                        
+                        self.ejecutable.listaInstrucciones.append(instruccionNueva)
+                        self.ejecutable.listaInstruccionesCodFuente.append(linea)
+                    
+                    else:
+                        self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametro no valido - Debe ser un registro o numero."
+
+                # Si es Inc, Dec o Pop
+                else:
+                    #Obtengo los parametros
+                    match = re.search('^(ax|bx|cx|dx)\s*$', parametros)
+                    if(match):
+                        if(instruccion == 'inc'):
+                            instruccionNueva = Inc(match.group(1))
+                        
+                        elif(instruccion == 'dec'):
+                            instruccionNueva = Dec(match.group(1))
+                        
+                        elif(instruccion == 'pop'):
+                            instruccionNueva = Pop(match.group(1))
+                        
+                        self.ejecutable.listaInstrucciones.append(instruccionNueva)
+                        self.ejecutable.listaInstruccionesCodFuente.append(linea)
+                    
+                    else:
+                        self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": Parametro no valido - Debe ser 'ax', 'bx', 'cx' o 'dx'"
+            
+            #Si dio error por instruccion mal escrita o no valida
+            else:
+                self.listaErrores[numeroDeLinea] = "Error en la linea " + str(numeroDeLinea) + ": instruccion no valida"
 
     
     def __mostrarErrores(self):
@@ -155,8 +184,14 @@ class Ensamblador:
             if(isinstance(instruccion, Mov) or isinstance(instruccion, Add) or isinstance(instruccion, Cmp)):
                 print(instruccion.nombre, "- con los parametros", "'" + instruccion.param1 + "' y", "'" + instruccion.param2 + "'")
             
-            elif(isinstance(instruccion, Dec) or isinstance(instruccion, Inc) or isinstance(instruccion, Jmp) or isinstance(instruccion, Jnz)):
+            elif(isinstance(instruccion, Dec) or isinstance(instruccion, Inc) or isinstance(instruccion, Jmp) or isinstance(instruccion, Jnz) or isinstance(instruccion, Push) or isinstance(instruccion, Pop)):
                 print(instruccion.nombre, "- con el parametro", "'" + instruccion.param1 + "'")
+            
+            elif(isinstance(instruccion, Call)):
+                print(instruccion.nombre, "- con los parametros", "'" + instruccion.param1 + "' y volver a posicion:", "'" + str(instruccion.posicionSiguiente) + "'")
+            
+            elif(isinstance(instruccion, Ret)):
+                print(instruccion.nombre)
             
         #Imprimo el diccionario lookupTable:
         print("\n", "Diccionario lookupTable:")
