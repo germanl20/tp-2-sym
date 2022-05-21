4088943b81f9aa490ea03ba78f4c9ef3d9a318d4
german Lopez <germanlopezz16@hotmail.com>
2022-05-21 19:54:04 -0300

    programaFibonacci.asm

--- programaFibonacci.asm
+++ programaFibonacci.asm
@@ -1,1 +1,32 @@
-Include "programaLibreriaFunciones.asm"
+Fibonacci:
+    Mov cx, 0
+    Cmp cx, ax
+    Jnz FibonacciUno
+    Ret
+FibonacciUno:
+    Mov cx, 1
+    Cmp cx, ax
+    Jnz FibonacciCalculo
+    Ret
+FibonacciCalculo:
+    Mov bx, ax
+    Dec ax
+    Dec bx
+    Dec bx
+    Push bx
+    Call Fibonacci
+    Mov dx, ax
+    Pop bx
+    Mov ax, bx
+    Push dx
+    Call Fibonacci
+    Pop dx
+    Add ax, dx
+    Ret
+
+Entry_point:
+    Mov ax, 4
+    Call Fibonacci
+    Mov bx, 0
+    Mov cx, 0
+    Int 1
